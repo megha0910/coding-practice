e47a96f99c6123fae578a67428198a41b07cce86
MEGHA k <138343296+megha0910@users.noreply.github.com>
2024-08-11 19:59:14 +0530

    Add files via upload

---
+++ datatype conversion.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#string
+name="Megha"
+type(name)
+
+
+# In[3]:
+
+
+#integer
+birth_year=2002
+type(birth_year)
+
+
+# In[6]:
+
+
+#conversion of interger to string
+birth_year=str(birth_year)
+type(birth_year)
+
+
+# In[7]:
+
+
+user_id=name+birth_year
+user_id
+
+
+# In[8]:
+
+
+#float
+weight=53.4
+type(weight)
+
+
+# In[9]:
+
+
+#conversion of float to integer
+weight=int(weight)
+type(weight)
+
+
+# In[10]:
+
+
+total_marks=80
+#conversion of interger to float
+total_marks=float(total_marks)
+type(total_marks)
+
+
+# In[ ]:
+
+
+
+
